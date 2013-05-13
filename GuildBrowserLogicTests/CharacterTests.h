fce352c9fb31bb75dfddbadb63fab86b105491a3
Piotr Dudzicz <pdudzicz@gmail.com>
2013-05-13 18:16:59 +0200

    Additional tests added

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Piotr Dudzicz on 5/8/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
